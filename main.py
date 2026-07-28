24a22c86ef3a832db17d1086ff05088226099005
Saurabh-827 <saurabhrabta@gmail.com>
2026-07-28 14:53:40 +0530

    feat: add broker connection state validation and code cleanup
    - Added 'is_broker_connected' global flag to track login state.
    - Secured /set-position endpoint to return 401 Unauthorized if broker is not connected.
    - Refined and corrected comments across the codebase for readability.

--- main.py
+++ main.py
@@ -49,11 +49,11 @@ async def lifespan(app: FastAPI):
             # Save it locally for next time
             with open(file_path, "w") as f:
                 json.dump(instrument_list, f)
-            print(f"Success: Downloaded and saved {len(instrument_list)} instruments into locally!")
+            print(f"Success: Downloaded and saved {len(instrument_list)} instruments locally!")
         except Exception as e:
             print(f"Error while loading Scrip Master {e}")
     else:
-        # load from local file
+        # Load from local file
         try:
             with open(file_path, "r") as f:
                 instrument_list = json.load(f)
@@ -64,7 +64,6 @@ async def lifespan(app: FastAPI):
     yield  # Here API goes on running
 
     # Shutdown logic - on app close
-
     print("Clear instrument memory...")
     instrument_list.clear()
 
@@ -100,50 +99,53 @@ class SetTargetSLRequest(BaseModel):
     sl: float
 
 # WebSocket Live Data State
-
 liv_market_data = {}
 
-# active positions 
+# Active positions 
 active_positions = {}
 
+# Flag to check login status
+is_broker_connected = False
+
 def start_websocket_stream(jwt_token, feed_token):
-    #initializing websocket instance
+    # Initializing websocket instance
     sws = SmartWebSocketV2(jwt_token, API_KEY, CLIENT_ID, feed_token)
 
-    # Define callbacks INSIDE so they can use 'sws' automatically (crashing websocket)
+    # Define callbacks INSIDE so they can use 'sws' automatically
     def on_data(wsapp, message):
-        # Tick Data Parsin: On getting new tick, read it and save in state
+        # Tick Data Parsing: On getting a new tick, read it and save it in state
         raw_token = message.get("token")
         if not raw_token:
             return
 
         token = raw_token
         
-        current_price = message.get("last_traded_price", 0)/100  # diveded by 100 to convert paisa in rupees
+        current_price = message.get("last_traded_price", 0) / 100  # Divided by 100 to convert paise to rupees
         liv_market_data[token] = current_price
-        # print(f"Live Price [{token}] : {current_price}")  # commented this for a clear terminal
+        # print(f"Live Price [{token}] : {current_price}")  # Commented this for a cleaner terminal
 
         # --- FAKE SPIKE FILTER LOGIC ---
         if token in active_positions and active_positions[token]['status'] == 'ACTIVE':
-            order =  active_positions[token]
+            order = active_positions[token]
 
-            # Condition 1: Check if target price or stop loss hitted 
+            # Condition 1: Check if target price or stop loss is hit 
             if current_price >= order["target"] or current_price <= order['sl']:
-                # if hitted first time than time will be noted 
+                
+                # If hit for the first time, record the breach time 
                 if order['breach_time'] is None:
                     order['breach_time'] = time.time()
                     print(f"[{token}] ALERT: Price reached {current_price}. Verification started...")
 
-                # if reached before than check time passed
+                # If already breached, check elapsed time
                 else:
                     time_elapsed = time.time() - order["breach_time"]
                     
-                    if time_elapsed >= 2.5: # 2.5 seconds sustai
+                    if time_elapsed >= 2.5: # 2.5 seconds sustained
                         print(f"[{token}] CONFIRMED: Price sustained at {current_price} for 2.5s. Executing REAL EXIT!")
                         active_positions[token]["status"] = "EXITED"
-                    # here we have to send order to broker
+                        # Here we will send the order to the broker
             else:
-                # Condition 2: if price comes again where it was than ( Fake Spike )
+                # Condition 2: If price returns to normal range (Fake Spike)
                 if order["breach_time"] is not None:
                     print(f"[{token}] FAKE SPIKE DETECTED & IGNORED! Price returned to {current_price}.")
                     order["breach_time"] = None # Time reset
@@ -151,8 +153,8 @@ def start_websocket_stream(jwt_token, feed_token):
     def on_open(wsapp):
         print("Websocket connected successfully")
         # Token Subscription:
-        # Exchange type : 5 (for MCX), 2(NSEFO), 1(NSE)
-        subscription_list = [{"exchangeType": 5, "tokens" : ["573628"]}]  # token IDs are specfic, currently taking dummy ID (sbi)
+        # Exchange type: 5 (MCX), 2 (NSEFO), 1 (NSE)
+        subscription_list = [{"exchangeType": 5, "tokens" : ["573628"]}]  # Token IDs are specific, currently taking a dummy ID
         sws.subscribe("spike_filter_stream", 1, subscription_list)
 
     def on_error(wsapp, error):
@@ -166,7 +168,7 @@ def start_websocket_stream(jwt_token, feed_token):
     sws.on_error = on_error
     sws.on_close = on_close
 
-    # connect function is blocking , thats why we call it in thread 
+    # Connect function is blocking, that's why we call it in a thread 
     sws.connect()
 
 @app.get("/")
@@ -175,6 +177,7 @@ def home():
 
 @app.post("/login", response_model=LoginResponse)
 def login_broker():
+    global is_broker_connected
     try:
         # 1: Generating TOTP
         totp = pyotp.TOTP(TOTP_SECRET).now()
@@ -189,13 +192,15 @@ def login_broker():
         auth_token = login_data['data']['jwtToken']
         feed_token = smartApi.getfeedToken()
 
-        #  Starting WebSocket in a separate thread
+        # Starting WebSocket in a separate thread
         ws_thread = threading.Thread(
-            target= start_websocket_stream,
-            args= (auth_token, feed_token),
-            daemon= True  # Daemon=True means when FastAPI closes thread also stop processing
+            target=start_websocket_stream,
+            args=(auth_token, feed_token),
+            daemon=True  # Daemon=True means when FastAPI closes, the thread also stops processing
         )
         ws_thread.start()
+        
+        is_broker_connected = True
 
         return {
             "status": "success",
@@ -209,19 +214,18 @@ def login_broker():
         raise HTTPException(status_code=500, detail=f"Login Failed: {str(e)}")
 
 @app.get("/search-token")
-def search_token(symbol:str, exchange: str="NSE"):
+def search_token(symbol: str, exchange: str = "NSE"):
     if not instrument_list:
         raise HTTPException(status_code=500, detail="Instrument list not loaded yet")
 
     # List Comprehension to find matching symbols 
     # We can check partial match and exact match 
-
     results = []
     for item in instrument_list:
         if symbol.upper() in item['symbol'].upper() and item['exch_seg'] == exchange.upper():
             results.append({
                 "symbol": item['symbol'],
-                "token" : item['token'],
+                "token": item['token'],
                 "exchange": item['exch_seg'],
                 "expiry": item.get('expiry', 'NA')
             })
@@ -239,6 +243,9 @@ def set_position(data: SetTargetSLRequest):
     """
     Sets active monitoring parameters (Target & SL) for a specific token.
     """
+    if not is_broker_connected:
+        raise HTTPException(status_code=401, detail="Broker not connected. Please login first by hitting /login endpoint.")
+    
     active_positions[data.token] = {
         "target": data.target,
         "sl": data.sl,
